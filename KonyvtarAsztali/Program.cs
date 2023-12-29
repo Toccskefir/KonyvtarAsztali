2d14b18c73cdc2d64ef5b2659d5fdf49c7257cbc
Tóth Kevin <tothkevin0716@gmail.com>
2023-12-29 16:51:51 +0100

    Created Program class

---
+++ KonyvtarAsztali/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace KonyvtarAsztali
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Statisztika statistic = new Statisztika();
+            if (args.Contains("--stat"))
+            {
+
+            }
+            else
+            {
+                Application app = new Application();
+                app.Run(new MainWindow());
+            }
+        }
+    }
+}
